2833683d9c59f0ec8951623f45f9d3b9b909b586
MAIA332 <lk.maia0505@outlook.com>
2023-12-14 08:42:21 -0300

    terminal handle

--- manage.py
+++ manage.py
@@ -1,47 +1,54 @@
+# ========================= imports ======================
 import datetime
 from colorama import Fore, Back, Style, init
 import platform
 import json
 import os
-import keyboard
 import threading
 import time
 from visual import graph
+import readline
 
-basedir = os.getcwd()
-with open('data/comandos.json') as file:
+# ========================= Classe para manipulação de histórico ======================
+class CommandHistory:
+    def __init__(self):
+        self.history = []
+        self.index = 0
+
+    def add_command(self, command):
+        self.history.append(command)
+        self.index = len(self.history)
+
+    def get_previous_command(self):
+        if self.index > 0:
+            self.index -= 1
+        return self.history[self.index] if self.history else ""
+
+    def get_next_command(self):
+        if self.index < len(self.history):
+            self.index += 1
+        return self.history[self.index] if self.history else ""
+
+# ========================= Configurações de sistema ======================
+basedir = os.getcwd() ## Diretorio Base
+with open('data/comandos.json') as file: # Pega a lista de comandos
     comandos = json.load(file)
 
-osParams ={
+osParams ={ # Parametros do S.O
     "SO":platform.system()
 }
 
-
-def key_listener():
-    while True:
-        try:
-            char = window.getch()
-            if keyboard.is_pressed('up'):
-
-                with open('data/history.txt','r') as file:
-                    lines = file.readlines()
-                
-                last_line = lines[-1].strip()
-                print(last_line)
-                
-            time.sleep(0.1)
-        except:
-            break
-
+# ========================= Função principal ======================
 
 def console():
+    history_ = CommandHistory() # Instancia a classe que manipula o histórico de comandos
     
 
-    def now(flag):
+    def now(flag): # Imprime data e hora atual
         a=datetime.datetime.now()
         print(a)
 
-    def help(flag):
+    def help(flag): # Exibe uma lista de comandos para verificar quais estão disponíveis
 
         def complete():
             command = input("~Lk (Comando): ")
@@ -61,7 +68,7 @@ def console():
             for i in comandos:
                 print(f"{Fore.RED}{i}{Style.RESET_ALL}: {comandos[i]['desc']}\n")
 
-    def clear(flag):
+    def clear(flag): # Função para limpeza da tela e limpeza de arquivos e cache
         def clear_history():
             with open('data/logs/log_comands.txt','w') as file:
                 file.write("")
@@ -80,13 +87,13 @@ def console():
             else:
                 os.system("cls")
 
-    def history(flag):
+    def history(flag): # Função para olhar o histórico de comandos
         with open('data/logs/log_comands.txt','r') as file:
             for line in file:
                 print(line)
             print("\n")
 
-    def display(flag):
+    def display(flag): # Função de exibição de dados, que estão na pasta data
     
         def sample_():
             with open('data/sample.json') as file:
@@ -111,7 +118,7 @@ def console():
             print(f"Comando {flag} é desconhecido ou inválido neste contexto {Fore.RED}{type(e)}{Style.RESET_ALL}")
             return
 
-    def build(flag):
+    def build(flag): # Função que instância o contrutor de gráficos, baseado na amostra indicada no comando
 
         def graph_():
 
@@ -131,7 +138,7 @@ def console():
                 }
 
                 cmd =  input(f"~Lk ({amostra}): ")
-                if cmf == "quit":
+                if cmd == "quit":
                     return
                 else:
                     t_graph[cmd]()
@@ -150,7 +157,7 @@ def console():
 
 
 
-    t = {
+    t = { # Lista de comandos disponíveis
         "clear":clear,
         "display":display,
         "now":now,
@@ -159,30 +166,41 @@ def console():
         "build":build
     }
 
+    def completer(text, state): # Definição dos dados utilizados na função tab, para autocompletar
+        options = [key for key in t.keys() if key.startswith(text)]
+        return options[state] if state < len(options) else None
 
-    while True:
-        try:
-            cmd = input("~Lk: ") + " "
-            
-            if cmd == " ":
-                pass
+    readline.set_completer(completer) # Define o completer da função tab
+    readline.parse_and_bind("tab: complete")
+
+    while True: #Loop principal, para pegar os comandos e separar as flags, bem como registrar nos históricos a interação do usuário
+        
+            user_input = input("~Lk: ") + " "
+
+            if user_input == "exit":
+                break
+
+            if user_input:
+                history_.add_command(user_input)
+
+            if user_input == "up":
+                command = history_.get_previous_command()
+            elif user_input == "down":
+                command = history_.get_next_command()
             else:
-                flags = cmd.split(" ")
+                command = user_input
 
-                with open('data/history.txt','a+') as file:
-                    file.writelines("\n"+f" ".join(flags))
-                    file.close()
+            flags = command.split(" ") 
 
-                with open('data/logs/log_comands.txt','a+') as file:
-                    file.writelines("\n"+f":{datetime.datetime.now()} ".join(flags))
-                    file.close()
+            with open("data/history.txt","a+") as file:
+                file.writelines("\n"+flags[0])       
 
-                t[flags[0]](flags[1])
+            with open('data/logs/log_comands.txt','a+') as file:
+                file.writelines("\n"+f":{datetime.datetime.now()} ".join(flags))
+                file.close()
 
-        except Exception as e:
-            print(f"Comando {cmd}é desconhecido ou inválido neste contexto {Fore.RED}{type(e)}{Style.RESET_ALL}")
+            t[flags[0]](flags[1])
 
-key_listener_thread = threading.Thread(target=key_listener)
-key_listener_thread.start()
-console()
-key_listener_thread.join()
+       
+if __name__ == "__main__": # Executa o arquivo se for chamado
+    console()
